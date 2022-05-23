242b9fbbbc3f860893567e9fd9a23477d31b6e52
EA <elkim018@gmail.com>
2022-05-23 11:48:08 +0900

    2022-05-23 2교시

--- fultter/image_lightzoom_switch_app/lib/main.dart
+++ fultter/image_lightzoom_switch_app/lib/main.dart
@@ -7,7 +7,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +18,7 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
@@ -35,7 +35,7 @@ class _MyHomePageState extends State<MyHomePage> {
   late bool _lampSwitch;
   late String _lampSizeStatus;
 
-  void initState(){
+  void initState() {
     super.initState();
     _lampImage = 'images/lamp_on.png';
     _lampWidth = 150;
@@ -43,26 +43,79 @@ class _MyHomePageState extends State<MyHomePage> {
     _lampSize = true;
     _lampSwitch = true;
     _lampSizeStatus = 'samll';
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Image 확대 및 축소'),
-      ),
-      body: Center(
-        child: Column(
+        appBar: AppBar(
+          title: const Text('Image 확대 및 축소'),
+        ),
+        body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset(
-              _lampImage,
-              width: _lampWidth,
-              height: _lampHeight,
-            )
+            SizedBox(
+              width: 350,
+              height: 650,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    _lampImage,
+                    width: _lampWidth,
+                    height: _lampHeight,
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('램프크기'),
+                Switch(
+                    value: _lampSize,
+                    onChanged: (value) {
+                      setState(() {
+                        _lampSize = value;
+                        decisionBigSmall();
+                      });
+                    }),
+                Text('램프 온오프'),
+                Switch(
+                    value: _lampSwitch,
+                    onChanged: (value) {
+                      setState(() {
+                        _lampSwitch = value;
+                        decisionOnOff();
+                      });
+                    }),
+              ],
+            ),
           ],
         ),
-      ),
-    );
+        ),
+        );
+  }
+
+//Function
+  decisionBigSmall() {
+    if (_lampSizeStatus == 'small') {
+      _lampWidth = 300;
+      _lampHeight = 600;
+      _lampSizeStatus = 'large';
+    } else {
+      _lampWidth = 150;
+      _lampHeight = 300;
+      _lampSizeStatus = 'small';
+    }
+  }
+
+  decisionOnOff() {
+    if (_lampSwitch) {
+      _lampImage = 'images/lamp_on.png';
+    } else {
+      _lampImage = 'images/lamp_off.png';
+    }
   }
 }
